757c7750238adda85feca1b20575d4c6751ce8c1
Eddwiin <maguran.mahendrarajan@gmail.com>
2020-10-31 15:12:03 +0100

    insert in the array exercice done

---
+++ sort/insert.js
@@ -0,0 +1,26 @@
+(function () {
+
+    // Write a function taking a arr, index and value then add in the arr according to index
+    
+    // What I have to do ?
+    // Create a function insert(arr,index,val)
+    // Verify into the function that's index is between 0 and arr.length - 1
+    // Create a for from arr.length - 1 to index
+    // Do arr arr[i +1] = arr[i]
+    // If n === i then arr[n + 1] = val
+    // Return arr;
+
+
+    const insert = (arr, index, val) => {
+        if (index < 0 && index > arr.length) return false;
+
+        for (let i = arr.length - 1; i > index; i--) {
+            arr[i+1] = arr[i];
+        }
+
+        arr[index + 1] = val;
+        return arr;
+    }
+
+    console.log(insert([1,5,6,8,7,4,1], 0, 17))
+})()
--- sort/insertion-sort.js
+++ sort/insertion-sort.js
@@ -1,16 +1,15 @@
-(function() {
+(function () {
 
     // Exercice by Colt Steel
     // Write a function to sort an arry by using insert sort
-    
 
-    // Qu'est-ce que je dois faire ?
-    // Je dois créer une fonction insertionSort(arr) et qui dois retourner arr trié
-    // Je vais parcourir mon arr de i = 1 à i < arr.length
+
+    // What do I have to do ?
+    // I have to create an insertSort (arr) function and that must return arr sorted
+    // I will iterate over my arr from i = 1 to i <arr.length
     // Create position = i
-    // Tant que arr[position] < arr[position-i] && position = 0
-    // Faire interchanger leur valeur et déincrémenter position
-
+    // As long as  position = 0
+    // Interchange their value and deincrement position
 
     const insertionSort = (arr) => {
 
@@ -19,8 +18,8 @@
             while (position !== 0) {
                 let isSwap = false;
 
-                if (arr[position] < arr[position-1]) {
-                    let temp = arr[position-1];
+                if (arr[position] < arr[position - 1]) {
+                    let temp = arr[position - 1];
                     arr[position - 1] = arr[position];
                     arr[position] = temp;
                     isSwap = true;
@@ -36,6 +35,6 @@
         return arr;
     }
 
-    console.log(insertionSort([4,5,8,1,2,6]));
-    console.log(insertionSort([2,1,9,76,4]));
+    console.log(insertionSort([4, 5, 8, 1, 2, 6]));
+    console.log(insertionSort([2, 1, 9, 76, 4]));
 })()
